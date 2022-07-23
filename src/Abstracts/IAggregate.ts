5e12201fb649f8a94874ff449473d2f5dbc66510
Jeremy Scott <jeremy@jeremyscott.io>
2022-07-23 17:04:41 +1000

    Introduce Stakeholders concept

--- src/Abstracts/IAggregate.ts
+++ src/Abstracts/IAggregate.ts
@@ -1,8 +1,10 @@
 import Guid from '../ValueObjects/Guid';
-import {IEvent} from "./IEvent";
+import {IEvent} from './IEvent';
+import Stakeholders from './Stakeholders';
 
 export default interface IAggregate{
     Id: Guid;
+    Stakeholders?: Stakeholders;
     GetEvents: () => IEvent[];
     apply: (event:IEvent) => void;
 };
